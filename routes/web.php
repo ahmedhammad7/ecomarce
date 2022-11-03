c10fe9a95f16ee01c4e462d8f1218a2625dd792e
unknown <you@example.com>
2022-11-03 06:01:42 +0200

    =first commit

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use GuzzleHttp\Middleware;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/landing', function () {
+    return view(view: 'landing');
+});
+
+Auth::routes(['verify' => true]);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->Middleware('verified');
+Route::get('/', function () {
+    return 'home';
+});
